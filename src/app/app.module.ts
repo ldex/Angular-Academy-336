c65e9cf3822d0daf025617c0ad21aadf497926e3
ldex <laurent.duveau+git@gmail.com>
2022-11-22 14:13:13 -0500

    Fin de la formation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
